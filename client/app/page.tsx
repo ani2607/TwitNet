32e3ba0e7f43de63eafb84606b309ebf914c59d6
Aniket Sharma <aniket220603@gmail.com>
2024-07-24 22:47:37 +0530

    initialized_client

---
+++ client/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+
+      <div className="grid grid-cols-12 h-screen w-screen">
+
+          <div className="col1 col-span-3">
+            
+          </div>
+          <div className="col2 col-span-6">hello</div>
+          <div className="col3 col-span-3">by</div>
+      </div>
+      
+    </main>
+  );
+}
